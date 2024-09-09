1dc6c37007af6908a9ba6cf430863a92bb2fbddd
Manuel <000balderas@gmail.com>
2024-09-09 11:12:26 -0600

    actualizacion de imagenes

--- ModulosJS/main.js
+++ ModulosJS/main.js
@@ -14,28 +14,70 @@ let divCartas = document.getElementsByClassName("cartas")[0]
 
 function crearApp() {
     // Obtenemos el contenedor donde se crearan las aplicaciones
-    let card1 = document.getElementsByClassName("card1")[0];
+    let card1 = document.getElementsByClassName("card1")[0];    
     let card2 = document.getElementsByClassName("card2")[0];
     let card3 = document.getElementsByClassName("card3")[0];
 
-    // Creamos el contenedor de la imagen
-    let appIMG = document.createElement("div");
+    
 
 
+    // Creamos todo el contenido
+    let contenedorIMG1 = document.createElement("div"); // agregar img por cada aplicacion
+    let contenedorIMG2 = document.createElement("div"); // agregar img por cada aplicacion
+    let contenedorIMG3 = document.createElement("div"); // agregar img por cada aplicacion
+
+    let appImagen1 = document.createElement("img");
+    let appImagen2 = document.createElement("img");
+    let appImagen3 = document.createElement("img");
+
+    let contenedorINFO1 = document.createElement("div");
+    let contenedorINFO2 = document.createElement("div");
+    let contenedorINFO3 = document.createElement("div");
+
+    let botonINFO1 = document.createElement("a");
+    let botonINFO2 = document.createElement("a");
+    let botonINFO3 = document.createElement("a");
 
 
+    // Asignamos clases
+    contenedorIMG1.classList.add("contenedorIMG1")
+    contenedorIMG2.classList.add("contenedorIMG2")
+    contenedorIMG3.classList.add("contenedorIMG3")
 
+    appImagen1.classList.add("appImg1");
+    appImagen2.classList.add("appImg2");
+    appImagen3.classList.add("appImg3");
 
-    let imagenAPP = document.createElement("img");
-    // Creamos el contenedor de informacion
-    let divINFO = document.createElement("div")
-    // Creamos el ancla de informacion
-    let aINFO = document.createElement("a")
+    contenedorINFO1.classList.add("contenedorINFO1");
+    contenedorINFO2.classList.add("contenedorINFO2");
+    contenedorINFO3.classList.add("contenedorINFO3");
 
-    // Agregamos contenido
-    imagenAPP.src = "./imagenes2/card1.jpg"
-    aINFO.textContent = "Mas informacion";
-    aINFO.href = "# "
+    // Agregamos el contenido a los contenedores de las cartas 
+    appImagen1.src = "../imagenes2/card2.webp"
+    appImagen1.alt = "Imagen carta1"
+    appImagen2.src = "../imagenes2/card1.jpg"
+    appImagen2.alt = "Imagen carta2"
+    appImagen3.src = "../imagenes2/juego1.jpg"
+    appImagen3.alt = "Imagen carta3"
+    
+    contenedorIMG1.appendChild(appImagen1);
+    contenedorIMG2.appendChild(appImagen2);
+    contenedorIMG3.appendChild(appImagen3);
+
+    // Agregamos hijos a contenedor card1
+    card1.appendChild(contenedorIMG1);
+    card1.appendChild(contenedorINFO1);
+    card1.appendChild(botonINFO1);
+    // Agregamos hijos a contenedor card2
+    card2.appendChild(contenedorIMG2);
+    card2.appendChild(appImagen2);
+    card2.appendChild(contenedorINFO2);
+    card2.appendChild(botonINFO2);
+    // Agregamos hijos a contenedor card1
+    card3.appendChild(contenedorIMG3);
+    card3.appendChild(appImagen3);
+    card3.appendChild(contenedorINFO3);
+    card3.appendChild(botonINFO3);
 
 }
 
@@ -53,7 +95,7 @@ function seccion1(titulo, parrafo, textBotonStarted) {
     divParrafo.textContent = parrafo
     divStarted.textContent = textBotonStarted
     divStarted.href = "#"
-    botonDC.src = "../imagenes2/botonDC.png"
+    botonDC.src = "imagenes2/botonDC.png"
 
     tituloS1.appendChild(divTitulo)
     parrafoS1.appendChild(divParrafo)
